c0cd174e0fd3dbc7a8882453de1d97f56e1ec209
vishnusai13 <2200090229@kluniversity.in>
2024-01-18 15:18:15 +0530

    HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+/** 
+ * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+     int num1 = 55, num2 = 15, sum,sub;
+    sum = num1 + num2;
+    sub = num1 - num2;
+    System.out.println("Subtraction of 2 numbers: "+sub);
+    System.out.println("Sum of these numbers: "+sum);
+    System.out.println("Hello, World");
+    System.out.println("Build, Test, Deploy");
+    System.out.println("Hi Harsha");
+    System.out.println("jenkins-Continuous Integration");
+  }
+}
